b850ccfc0f264fb69f9fbdf28d69edfc15161ee8
Arturo <arturo.amb89@gmail.com>
2020-09-29 21:17:45 -0300

    Database connection.

--- app.js
+++ app.js
@@ -11,3 +11,30 @@ require("dotenv").config();
 
 // app - express
 const app = express();
+
+// modern connection
+const db = async () => {
+    try {
+        const success = await mongoose.connect(process.env.DATABASE, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true, 
+            useFindAndModify: false
+        });
+        console.log('DB Connected');
+    } catch (error) {
+        console.log('DB Connection Error', error);
+    }
+}
+
+// execute db connection
+db();
+
+// port
+const port = process.env.PORT || 8000;
+
+// listen port
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
